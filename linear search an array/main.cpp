b57a49289e27bbdd2393a1cb5818766a8c851d5d
maya-nachiappan <maya.n102304@gmail.com>
2024-10-07 15:54:19 -0700

    created a linear search function using a for and if loop, which returns -1 if the value is not found in the array. if it is found, returns index position

--- linear search an array/main.cpp
+++ linear search an array/main.cpp
@@ -6,9 +6,46 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int linearSearch(int arr[], int size, int target) 
+{
+    for (int i = 0; i < size; ++i) {
+        if (arr[i] == target) {
+            return i; // Return the index if the target is found
+        }
+    }
+    return -1; // Return -1 if the target is not found
+}
+
+int main() 
+{
+    int size;
+
+    // Ask the user for the number of elements
+    cout << "Enter the number of elements in the array: " << endl;
+    cin >> size;
+
+    // Create an array of the specified size
+    int* arr = new int[size];
+
+    // Ask the user to input the elements
+    cout << "Enter " << size << " integers:" << endl;
+    for (int i = 0; i < size; ++i) {
+        cin >> arr[i];
+    }
+
+    int target;
+    cout << "Enter the value to search for: " << endl;
+    cin >> target;
+
+    // uses linear search function previously created
+    int index = linearSearch(arr, size, target);
+
+    if (index != -1) {
+        cout << "Value " << target << " found at index: " << index << endl;
+    } else {
+    cout << "Value " << target << " not found in the array." << endl;
+    }
     return 0;
 }
