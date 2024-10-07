07f59fe8788941c786405200fedb0bd46c4b92c2
maya-nachiappan <maya.n102304@gmail.com>
2024-10-07 15:48:03 -0700

    Initial Commit

---
+++ linear search an array/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  linear search an array
+//
+//  Created by maya nachiappan on 10/7/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
